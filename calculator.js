f97f001b0317d7dafc0d39585acddc033f76ee6c
Nathan Griffin <nathanjgriffin@gmail.com>
2014-01-12 22:31:42 -0500

    simple little calculator ;)

---
+++ calculator.js
@@ -0,0 +1,72 @@
+angular.module('myApp',[])
+	.directive('calculator', function() {
+	    return {
+	        restrict: 'E',
+	        templateUrl: "template.html" ,
+	        scope: true,
+	        controller: function($scope, $parse) {
+	        	$scope.$parse = $parse;
+	        	$scope.fieldValues = {};
+	        	$scope.calculations = [];
+
+	        	$scope.calculate = function(calculationString, values){
+	       			try {
+	       				var value = $parse(calculationString)(values);
+	       				return (_.isFinite(value) ? value : 0);
+	       			} catch (e) {
+	       				return "Invalid expression :("
+	       			}
+	       		};
+
+	       		$scope.addField = function(name, value){
+	       			if (!_.isFinite(value)) {
+	       				value = 0;
+	       			}
+	       			if (!name){
+	       				name = "Field" + (_.keys($scope.fieldValues).length + 1);
+	       			}
+	       			$scope.fieldValues[name] = value;
+
+	       			$scope.newName = null;
+	       			$scope.newValue = null;
+	       		};
+	       		$scope.removeField = function(name) {
+	       			delete $scope.fieldValues[name] 
+	       		};
+	       		$scope.addCalculation = function(calculation, values) {
+	       			$scope.calculations.push({ 
+	   					string: calculation, 
+	   					value: $scope.calculate(calculation, values || $scope.getFieldValueNumbers()) 
+	       			});
+	       			$scope.newCalculation = null;
+	       		};
+	       	
+	       		$scope.getFieldValueNumbers = function(){
+	       			fieldValueNumbers = {};
+	       			_.each($scope.fieldValues, function(value, name){
+	       				var numberValue = Number(value);
+	       				fieldValueNumbers[name] = _.isFinite(numberValue) ? numberValue : 0;
+	       			});
+	       			return fieldValueNumbers;
+	       		}
+
+	       		$scope.$watch("fieldValues", function(){
+	       			var fieldValueNumbers = $scope.getFieldValueNumbers();
+	       			_.each($scope.calculations, function(calc) {
+	       				calc.value = $scope.calculate(calc.string, fieldValueNumbers);
+	       			});
+	       		}, true);
+	        },
+	        link: function(scope, element, attrs) {
+	        	var startingValues = scope.$parse(attrs.values)(); 
+	        	_.each(startingValues, function(value, key) {
+	        		scope.addField(key, Number(value));
+	        	});
+	        	
+	        	var calculationStrings = _.isUndefined(attrs.calculations) ? [] : attrs.calculations.split(',');
+				_.each(calculationStrings, function(calculation){
+					scope.addCalculation(calculation, scope.fieldValues);
+				});
+	        }
+	    }
+});
